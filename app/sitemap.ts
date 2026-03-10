ba10c9cfd5cfc77091c3ed503a1ed5bce34979ca
praymond24 <peterraymond5789@gmail.com>
2026-03-09 22:39:47 -0400

    Update sitemap with blog post

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 1,
     },
+    {
+      url: 'https://spendtrap.com/blog/27-subscriptions-you-dont-need',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
   ]
 }
